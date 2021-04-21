d1839907196490bc0b03a6f8be8501156c43ea1f
leesohyun13 <thgus5614@naver.com>
2021-04-21 15:05:01 +0900

    Add KakaoCafeApi interface and Add rest api key on local.properties

---
+++ app/src/main/java/com/sohyun/coroutinesampleapp/data/network/KakaoCafeApi.kt
@@ -0,0 +1,31 @@
+package com.sohyun.coroutinesampleapp.data.network
+
+import com.sohyun.coroutinesampleapp.BuildConfig
+import com.sohyun.coroutinesampleapp.data.KakaoSearchSortType
+import com.sohyun.coroutinesampleapp.data.remote.model.KaKaoCafeResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoCafeApi {
+    @GET(SUB_PATH_CAFE)
+    fun searchCafe(
+        @Header(SCHEMA_REQUEST_HEADER_AUTHORIZATION) restApiKey: String = BuildConfig.KAKAO_CAFE_REST_API_KEY,
+        @Query(SCHEMA_QUERY) query: String,
+        @Query(SCHEMA_QUERY_SORT) sort: KakaoSearchSortType?,
+        @Query(SCHEMA_QUERY_PAGE) page: Int? = BOOK_STARTING_PAGE_INDEX,
+        @Query(SCHEMA_QUERY_SIZE) size: Int = BOOK_PAGING_SIZE,
+    ): KaKaoCafeResponse
+
+    companion object {
+        const val SUB_PATH_CAFE = "/v2/search/cafe"
+        const val BOOK_STARTING_PAGE_INDEX = 1
+        const val BOOK_PAGING_SIZE = 50
+
+        const val SCHEMA_REQUEST_HEADER_AUTHORIZATION = "Authorization"
+        const val SCHEMA_QUERY = "query"
+        const val SCHEMA_QUERY_SORT = "sort"
+        const val SCHEMA_QUERY_PAGE = "page"
+        const val SCHEMA_QUERY_SIZE = "size"
+    }
+}
