f218b4e592820fb23c96dbe98fc7384790951ded
thgus5614@naver.com <thgus5614@naver.com>
2021-04-23 22:11:49 +0900

    Add framelayout and Applay MainFragment() on MainActivity.kt

--- app/src/main/java/com/sohyun/coroutinesampleapp/ui/MainActivity.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.sohyun.coroutinesampleapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sohyun.coroutinesampleapp.R
+import com.sohyun.coroutinesampleapp.ui.main.MainFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment())
+            .commitNow()
     }
 }
