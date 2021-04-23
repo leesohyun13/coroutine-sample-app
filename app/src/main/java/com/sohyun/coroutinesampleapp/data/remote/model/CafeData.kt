b4b44c8ebbee4e57519d87d1c51eaf0671fdda35
thgus5614@naver.com <thgus5614@naver.com>
2021-04-23 22:30:16 +0900

    Add CafeData.kt

---
+++ app/src/main/java/com/sohyun/coroutinesampleapp/data/remote/model/CafeData.kt
@@ -0,0 +1,13 @@
+package com.sohyun.coroutinesampleapp.data.remote.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CafeData(
+    val title: String,
+    val contents: String,
+    val url: String,
+    val cafename: String,
+    val thumbnail: String,
+    val datetime: String, // 카페 글 작성시간, ISO 8601 형식, [YYYY]-[MM]-[DD]T[hh]:[mm]:[ss].000+[tz]
+)
