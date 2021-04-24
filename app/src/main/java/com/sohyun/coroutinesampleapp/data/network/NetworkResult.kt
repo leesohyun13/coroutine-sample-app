e9dfa4395274456bc344a5ec2f56823f9d521477
thgus5614@naver.com <thgus5614@naver.com>
2021-04-24 17:26:49 +0900

    Add NetworkResult.kt

--- app/src/main/java/com/sohyun/coroutinesampleapp/data/network/NetworkResult.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/data/network/NetworkResult.kt
@@ -1,6 +1,13 @@
 package com.sohyun.coroutinesampleapp.data.network
 
-sealed class NetworkResult<out T> {
-    data class Success<out T>(val data: T) : NetworkResult<T>()
-    data class Error(val exception: Exception) : NetworkResult<Nothing>()
+import okhttp3.ResponseBody
+
+sealed class NetworkStatus<out T> {
+    data class Success<out T>(val data: T) : NetworkStatus<T>()
+    data class Failure(
+        val isNetworkError: Boolean,
+        val errorCode: Int?,
+        val errorBody: ResponseBody?
+    ) : NetworkStatus<Nothing>()
+    object Loading : NetworkStatus<Nothing>()
 }
