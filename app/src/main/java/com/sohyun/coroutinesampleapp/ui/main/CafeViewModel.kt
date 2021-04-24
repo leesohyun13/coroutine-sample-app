eba5e45038c241c7d9596cdc3f0dba80f8c4ac0b
thgus5614@naver.com <thgus5614@naver.com>
2021-04-24 19:16:28 +0900

    Add showProgressbar value and function that change search cafe item data class

--- app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/CafeViewModel.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/CafeViewModel.kt
@@ -1,8 +1,9 @@
 package com.sohyun.coroutinesampleapp.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sohyun.coroutinesampleapp.data.network.NetworkStatus
-import com.sohyun.coroutinesampleapp.data.remote.model.KaKaoCafeResponse
+import com.sohyun.coroutinesampleapp.data.remote.model.CafeData
 import com.sohyun.coroutinesampleapp.data.repos.KakaoCafeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,9 +12,30 @@ import javax.inject.Inject
 class CafeViewModel @Inject constructor(
     private val kakaoCafeRepository: KakaoCafeRepository
 ): ViewModel() {
+    val cafeList = MutableLiveData<List<CafeData>>()
 
-    suspend fun requestKakaoCafeSearch(query: String): NetworkStatus<KaKaoCafeResponse> {
-        return kakaoCafeRepository.getSearchResult(query, 1)
+    private val showProgressbar = MutableLiveData(false)
+    fun showProgressbar() = showProgressbar
+
+    suspend fun requestKakaoCafeSearch(query: String) {
+        val responses = kakaoCafeRepository.getSearchResult(query, 1)
+        when(responses) {
+            is NetworkStatus.Success -> {
+                showProgressbar.value = false
+                //
+                val list = responses.data.documents
+                    .map { CafeData(it.title, it.contents, it.url, it.cafename, it.thumbnail, it.datetime) }
+                    .toList()
+                cafeList.postValue(list)
+            }
+            is NetworkStatus.Failure -> {
+                // TODO show error
+                showProgressbar.value = false
+            }
+            is NetworkStatus.Loading -> {
+                showProgressbar.value = true
+            }
+        }
     }
 }
 
