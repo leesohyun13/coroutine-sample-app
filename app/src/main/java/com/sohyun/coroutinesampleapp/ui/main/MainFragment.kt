2379311d250f2b09e7507031a5a1ccea08a8e858
thgus5614@naver.com <thgus5614@naver.com>
2021-04-24 19:17:14 +0900

    Add CafeAdapter on MainFragment.kt

--- app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/MainFragment.kt
@@ -1,19 +1,22 @@
 package com.sohyun.coroutinesampleapp.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sohyun.coroutinesampleapp.R
 import com.sohyun.coroutinesampleapp.databinding.FragmentMainBinding
 import com.sohyun.coroutinesampleapp.ui.base.BaseFragment
+import com.sohyun.coroutinesampleapp.util.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment: BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     private val mainViewModel: CafeViewModel by viewModels()
+    private lateinit var cafeAdapter: CafeAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,10 +24,17 @@ class MainFragment: BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
             lifecycleOwner = this@MainFragment
             viewmodel = mainViewModel
 
+            cafeSearchRecyclerview.run {
+                cafeAdapter = CafeAdapter()
+                adapter = cafeAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+            }
+
             searchButton.setOnClickListener {
+                hideKeyboard()
                 lifecycleScope.launch {
-                    val cafeList = mainViewModel.requestKakaoCafeSearch(binding.cafeSearchEdittext.text.toString())
-                    Log.d("MainFragment", "onCreateView: cafeList ${cafeList}")
+                    mainViewModel.requestKakaoCafeSearch(binding.cafeSearchEdittext.text.toString())
                 }
             }
         }
