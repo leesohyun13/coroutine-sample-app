49f6e8a201fe2e60a7598d9d8414e8abc7e8b981
thgus5614@naver.com <thgus5614@naver.com>
2021-04-24 17:27:39 +0900

    Remove RemoteDataSource and Impl

--- app/src/main/java/com/sohyun/coroutinesampleapp/data/remote/RemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.sohyun.coroutinesampleapp.data.remote
-
-import com.sohyun.coroutinesampleapp.data.KakaoSearchSortType
-import com.sohyun.coroutinesampleapp.data.remote.model.KaKaoCafeResponse
-
-interface RemoteDataSource {
-    suspend fun searchCafes(query: String, page: Int?, sortType: KakaoSearchSortType?): KaKaoCafeResponse
-}
