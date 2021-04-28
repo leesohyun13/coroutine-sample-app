17f0e9983df3ef96f0e9006af1d41c446550a513
thgus5614@naver.com <thgus5614@naver.com>
2021-04-28 20:56:13 +0900

    Add function that create database instance

--- app/src/main/java/com/sohyun/coroutinesampleapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/data/local/AppDatabase.kt
@@ -1,9 +1,33 @@
 package com.sohyun.coroutinesampleapp.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.sohyun.coroutinesampleapp.data.model.CafeData
 
 @Database(entities = [CafeData::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun cafeDao(): CafeDao
+
+    companion object {
+        private const val DATABASE_NAME = "kakao-cafe.db"
+
+        @Volatile
+        private var INSTANCE : AppDatabase? = null
+
+        // 데이터베이스 생성시 단일 스레드에서만 작업하고, lock 걸림
+        operator fun invoke(context: Context) = INSTANCE ?: synchronized(this){
+            INSTANCE ?: buildDatabase(context).also {
+                INSTANCE = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 }
