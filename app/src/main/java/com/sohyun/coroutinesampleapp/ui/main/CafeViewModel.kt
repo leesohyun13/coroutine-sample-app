e44a1800ab976e654db3de74699f01b51eea9e7d
thgus5614@naver.com <thgus5614@naver.com>
2021-05-03 22:02:00 +0900

    Add search text on Local DB

--- app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/CafeViewModel.kt
+++ app/src/main/java/com/sohyun/coroutinesampleapp/ui/main/CafeViewModel.kt
@@ -4,16 +4,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sohyun.coroutinesampleapp.data.model.CafeData
+import com.sohyun.coroutinesampleapp.data.model.SearchData
 import com.sohyun.coroutinesampleapp.data.network.NetworkStatus
 import com.sohyun.coroutinesampleapp.data.repository.KakaoCafeRepository
+import com.sohyun.coroutinesampleapp.di.DefaultDispatcher
+import com.sohyun.coroutinesampleapp.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class CafeViewModel @Inject constructor(
-    private val kakaoCafeRepository: KakaoCafeRepository
+    private val kakaoCafeRepository: KakaoCafeRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ): ViewModel() {
     val cafeList = MutableLiveData<List<CafeData>>()
 
@@ -22,12 +29,13 @@ class CafeViewModel @Inject constructor(
 
     // TODO 이전 검색어 저장 및 다음 검색어와 비교
     suspend fun requestKakaoCafeSearch(query: String) {
-        val responses = kakaoCafeRepository.getSearchResult(query, 1)
-        when(responses) {
+        withContext(defaultDispatcher) { kakaoCafeRepository.saveSearchHistoryItem(SearchData(search = query)) }
+        val response = withContext(ioDispatcher) { kakaoCafeRepository.getSearchResult(query, 1) }
+        when(response) {
             is NetworkStatus.Success -> {
                 showProgressbar.value = false
                 //
-                val list = responses.data.documents
+                val list = response.data.documents
                     .map {
                         CafeData(
                             it.title,
@@ -58,7 +66,6 @@ class CafeViewModel @Inject constructor(
 
     fun removeLike(item: CafeData) {
         viewModelScope.launch(Dispatchers.Default) { kakaoCafeRepository.deleteItem(item) }
-
     }
 }
 
